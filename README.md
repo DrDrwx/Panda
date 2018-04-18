eb10e4efe14b71ad0fbc30dff09c1fabfe1201da
tommcdowell@protonmail.com <tommcdowell@protonmail.com>
2018-04-18 22:20:23 +0000

    Minor changes. Changelog to follow.

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # Panda
+
+## Description
+Panda is a module which I have developed to stop me rewriting code.
+So far it contains a Logger class.
+
+## Logger: Features
+Once initialised:
+```python
+logger = panda.Logger(logFile='panda.plog', verbose=True, write=True, debug=True)
+```
+The logger has the following options:
+```python
+- logger.log('Log message')
+- logger.error('Error message')
+- logger.debug('Debug message')
+- logger.warning('Warning message')
+- logger.success('Success message')
+```
+With example outputs:
+```text
+[2018-04-18 22:04:49] [-] Log message
+[2018-04-18 22:04:49] [!] Error: Error message
+[2018-04-18 22:04:49] [D] Debug: Debug message
+[2018-04-18 22:04:49] [W] Warning: Warning message
+[2018-04-18 22:04:49] [+] Success: Success message
+```
+
+Each of these functions have `verbose`, `write` and `logFile` flags preset.
+You can overwrite each of these to further control the logging, an example:
+
+```python
+logger.debug('Something happened...')
+```
+Each function comes with a timestamp by default.
+The above would output the following, for example:
+`[2018-04-18 22:04:49] [D] Debug: Something happened...`
+
+However, is it possible to only print these to the screen by doing:
+```python
+logger.debug('Something happened...', write=False)
+```
+.. or to only write these to the log file, one can:
+```python
+logger.debug('Something happened...', verbose=False)
+```
+
+It is further possible to change the output's type, using `logType`:
+```python
+logger.log('Something happened...', logType='plus')
+
+# Options include:
+# plus [+], info [-], error [!], debug [D], warn [W]
+```
+
+### All Options
+```python
+logger.log("Log message.", logType="error", write=False, verbose=True)
+# Options are set to true by default.
+```
--- panda.py
+++ panda.py
@@ -19,54 +19,56 @@ class Logger():
         self.verbose = verbose
         self.write = write
         self.logFile = logFile
+        self.debugger=debug
 
-    def log(self, logMsg, logType='info', verbose=True, write=True, logFile=self.logFile):
+    def log(self, logMsg, logType='info', verbose=True, write=True):
         from time import strftime as s, gmtime as g
         log = "%s %s %s" % (s(self.strf, g()), self.types[logType], logMsg)
         if self.verbose and verbose:
             print log
         if self.write and write:
-            lf = open(logFile, "a")
+            lf = open(self.logFile, "a")
             lf.write(log+"\n")
             lf.close()
 
-    def success(self, logMsg, logType='plus', verbose=True, write=True, logFile=self.logFile):
+    def success(self, logMsg, logType='plus', verbose=True, write=True):
+        from time import strftime as s, gmtime as g
+        log = "%s %s Success: %s" % (s(self.strf, g()), self.types[logType], logMsg)
+        if self.verbose and verbose:
+            print log
+        if self.write and write:
+            lf = open(self.logFile, "a")
+            lf.write(log+"\n")
+            lf.close()
+
+    def error(self, logMsg, logType='error', verbose=True, write=True):
         from time import strftime as s, gmtime as g
         log = "%s %s Error: %s" % (s(self.strf, g()), self.types[logType], logMsg)
         if self.verbose and verbose:
             print log
         if self.write and write:
-            lf = open(logFile, "a")
+            lf = open(self.logFile, "a")
             lf.write(log+"\n")
             lf.close()
 
-    def error(self, logMsg, logType='error', verbose=True, write=True, logFile=self.logFile):
-        from time import strftime as s, gmtime as g
-        log = "%s %s Error: %s" % (s(self.strf, g()), self.types[logType], logMsg)
-        if self.verbose and verbose:
-            print log
-        if self.write and write:
-            lf = open(logFile, "a")
-            lf.write(log+"\n")
-            lf.close()
+    def debug(self, logMsg, logType='debug', verbose=True, write=True):
+        if self.debugger:
+            from time import strftime as s, gmtime as g
+            log = "%s %s Debug: %s" % (s(self.strf, g()), self.types[logType], logMsg)
+            if self.verbose and verbose:
+                print log
+            if self.write and write:
+                lf = open(self.logFile, "a")
+                lf.write(log+"\n")
+                lf.close()
 
-    def debug(self, logMsg, logType='debug', verbose=True, write=True, logFile=self.logFile):
-        from time import strftime as s, gmtime as g
-        log = "%s %s Debug: %s" % (s(self.strf, g()), self.types[logType], logMsg)
-        if self.verbose and verbose:
-            print log
-        if self.write and write:
-            lf = open(logFile, "a")
-            lf.write(log+"\n")
-            lf.close()
-
-    def warning(self, logMsg, logType='warn', verbose=True, write=True, logFile=self.logFile):
+    def warning(self, logMsg, logType='warn', verbose=True, write=True):
         from time import strftime as s, gmtime as g
         log = "%s %s Warning: %s" % (s(self.strf, g()), self.types[logType], logMsg)
         if self.verbose and verbose:
             print log
         if self.write and write:
-            lf = open(logFile, "a")
+            lf = open(self.logFile, "a")
             lf.write(log+"\n")
             lf.close()
 
